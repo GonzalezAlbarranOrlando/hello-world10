128f1d599d0c9b74f56094f0de44d273638bc4d9
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-08-24 20:19:59 -0500

    yarn build

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Template in vue js</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data(){
+        return {
+            name: 'Orlando'
+        }
+    }
+}
+</script>
